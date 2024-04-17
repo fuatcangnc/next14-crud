9a7cbc722c2fe90588b013dea3b74f823dc79609
Fuat <fuatcankod@gmail.com>
2024-04-17 16:04:51 +0300

    vercel fail

--- app/api/create-table/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest,NextResponse } from "next/server";
-import { sql } from "@vercel/postgres";
-
-export async function GET(request: Request) {
-    const result = await sql`CREATE TABLE Grundes (NAME varchar(255))`;
-    return NextResponse.json({result})
-  }
